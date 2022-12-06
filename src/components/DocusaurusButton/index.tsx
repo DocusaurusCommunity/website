382a9c39d9f2130ae5e5c7e30282d06b568d0a43
Mikey O'Toole <mikey@homotechsual.dev>
2022-12-06 21:27:54 +0000

    Add admonitions documentation.

---
+++ src/components/DocusaurusButton/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.scss';
+import Link from '@docusaurus/Link';
+import DocusaurusLogo from '@site/static/img/docusaurus.svg'
+
+type Props = {
+    to: string;
+    label: string;
+};
+
+export default function DocusaurusButton(props: Props): JSX.Element {
+    return (
+        <div className={clsx(styles.docusaurusButton, 'text--center')}>
+            <p className="text--center">
+                <Link className={clsx(styles.docusaurusButtonButton, 'button button--secondary button--lg')} to={props.to}>
+                    <span className={styles.docusaurusButtonIcon}><DocusaurusLogo /></span>
+                    <span className={styles.docusaurusButtonText}>{props.label}</span>
+                </Link>
+            </p>
+        </div>
+    );
+}
