58eea2fba561b154e0650733704f12a33b3b924b
Mikey O'Toole <mikey@homotechsual.dev>
2023-11-11 11:09:05 +0000

    Add description to Docker Deployment page.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -210,7 +210,16 @@ const config = {
       },
     }),
   plugins: [
-    '@docusaurus/plugin-ideal-image',
+    [
+      '@docusaurus/plugin-ideal-image',
+      /** @type {import('@docusaurus/plugin-ideal-image').Options} */
+      {
+        quality: 100,
+        max: 1030, // max resized image's size.
+        min: 640, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+      },
+    ],
     'docusaurus-plugin-sass',
     [
       'docusaurus-plugin-plausible',
