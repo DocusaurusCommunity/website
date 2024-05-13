e6e4cbd9f6a873b1987446920de67095836817a8
Mikey O'Toole <mikey@homotechsual.dev>
2024-05-13 22:02:54 +0100

    Refactor component docs and finalise edits to Columns component.

--- src/components/Column/index.tsx
+++ src/components/Column/index.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode, CSSProperties } from 'react';
 import clsx from 'clsx'; 
 import MDXContent from '@theme/MDXContent';
-import styles from './styles.module.css';
+import styles from './styles.module.scss';
 
 interface ColumnProps {
   children: ReactNode;
