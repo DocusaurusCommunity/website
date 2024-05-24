1a2adf8714b04265dd465817662688c4823a8468
Docux <valentin.chevoleau@orange.fr>
2024-05-25 01:28:15 +0200

    add cardimage and demo an trick and tips docux

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -19,6 +19,11 @@ import Columns from '@site/src/components/Columns';
 import Column from '@site/src/components/Column';
 import Button from '@site/src/components/Button';
 import ColorPreview from '@site/src/components/ColorPreview';
+import Card from '@site/src/components/SimpleCard/Card';
+import CardBody from '@site/src/components/SimpleCard/CardBody';
+import CardFooter from '@site/src/components/SimpleCard/CardFooter';
+import CardHeader from '@site/src/components/SimpleCard/CardHeader';
+import CardImage from '@site/src/components/SimpleCard/CardImage';
 
 library.add(fab, fas);
 
@@ -34,4 +39,9 @@ export default {
     Column,
     Button,
     ColorPreview,
+    Card, 
+    CardHeader, 
+    CardBody, 
+    CardFooter,
+    CardImage,
 };
